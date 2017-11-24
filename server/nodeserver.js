fb2faf245f55758ff47ac34c139d5da755fb4c1d
Eric Smith <easmith03@comcast.net>
2017-11-23 21:37:26 -0500

    Add update, create, delete, read functions for person.

--- server/nodeserver.js
+++ server/nodeserver.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const message = require('./person.js');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/person', message);
 
--- server/person.js
+++ server/person.js
@@ -3,20 +3,158 @@
 const express = require('express');
 const router = express.Router();
 const mongo = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
 
-const messages = ['Lets Go', 'What are you going to do?', 'This move is critical', 'It is getting interesting', 'Did you leave yourself an opening?', 'Time is running out', 'This is it!', 'Almost too late', 'Game over', 'WHAT????'];
-// Home page route
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/mydb";
+
+
+// Get all
 router.get('/', function(req, res) {
-	console.log('REQ Message:', req.query.msg);
-	res.status(200);
+	//console.log('REQ Message:', req.query.msg);
 	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({resp: messages[parseInt(req.query.msg, 10)]}));
-	  //res.send(JSON.stringify({resp: 'hello'}));
+
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			return;
+		}
+  
+		db.collection("people").find({}).toArray(function(err, result) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				return;
+			}
+			res.status(200);
+			res.send(result);
+			console.log(result);
+			db.close();
+		});
+	});
 });
 
-// About page route
-router.get('/about', function(req, res) {
-  res.send('About Messages');
+//Get by id
+router.get('/:personId', function(req, res) {
+	//console.log('REQ Message:', req.query.msg);
+	res.setHeader('Content-Type', 'application/json');
+	
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			return;
+		}
+		db.collection("people").findOne(ObjectID(req.params.personId), function(err, result) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				return;
+			}
+			res.status(200);
+			res.send(result);
+			console.log(result);
+			db.close();
+		});
+	});
 });
 
+//Post insert new
+router.post('/', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			return;
+		}
+		
+		db.collection("people").insertOne(req.body, function(err, response) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				return;
+			}
+			res.status(201);
+			res.send();
+			db.close();
+		});
+	});
+});
+
+
+//Delete record
+router.delete('/:personId', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			return;
+		}
+		
+		db.collection("customers").deleteOne({_id: ObjectID(req.params.personId)}, function(err, obj) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				return;
+			}
+			res.status(200);
+			res.send();
+			db.close();
+		});
+	});
+});
+
+
+//Update record partial
+router.patch('/:personId', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			return;
+		}
+		
+		db.collection("people").updateOne({_id: ObjectID(req.params.personId)}, { $set: req.body} , function(err, obj) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				return;
+			}
+			res.status(204);
+			res.send();
+			db.close();
+		});
+	});
+});
+
+//Update record total
+router.put('/:personId', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			return;
+		}
+		
+		db.collection("people").updateOne({_id: ObjectID(req.params.personId)}, req.body, function(err, obj) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				return;
+			}
+			res.status(204);
+			res.send();
+			db.close();
+		});
+	});
+});
 module.exports = router;
